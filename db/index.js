51472a48a60615918b0fe85aeb2921526b7912b3
Hamza Bashir <hamzabashir218@gmail.com>
2023-06-26 20:14:23 +0500

    Mongodb integration.

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 const connectToDB = async () => {
-    mongoose.connect("mongodb+srv://hamzabashir218:hamzabashir218@cluster0.golz1jd.mongodb.net/").then(()=>{
+    // mongoose.connect("mongodb+srv://hamzabashir218:hamzabashir218@cluster0.golz1jd.mongodb.net/").then(()=>{
+        mongoose.connect(process.env.MONGODB_URI).then(()=>{
         console.log("Connection to database is established.")
     }).catch((error)=>{
         console.log(error)
