7f7c1a5c216592d27df01c7295b533ce86f30f43
Hamza Bashir <hamzabashir218@gmail.com>
2023-06-26 18:04:03 +0500

    vercel

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/hello",(req,res)=>{
-    res.status(200).json("Hello api")
+app.get("/",(req,res)=>{
+    res.status(200).json("Hello from home page.")
 })
 
-app.listen(3000,()=>{
-    console.log("Server listening on port 3000")
+app.get("/hello",(req,res)=>{
+    res.status(200).json("Hello from /hello")
+})
+
+app.listen(9001,()=>{
+    console.log("Server listening on port 9001")
 })
