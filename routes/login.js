7fa0118ca00594e6766d34f204bdc3268efdbd44
Hamza Bashir <hamzabashir218@gmail.com>
2023-07-03 16:48:11 +0500

    testing login api

--- routes/login.js
+++ routes/login.js
@@ -27,27 +27,50 @@ router.post("/login", async (req, res) => {
       .json({ error: "Please enter a valid email address" });
 
   try {
+    const reqBody = {
+        dataSource: "Cluster0",
+        database: "myFirstDatabase",
+        collection: "users",
+        filter: {
+            email: email
+        },
+      }
+    const res = await fetch(
+          "https://data.mongodb-api.com/app/data-hsnwi/endpoint/data/v1/action/findOne",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              apiKey:
+                "ESm4aIVDOmymuXX1C651AhrEBhk1X4u8y7ApkMejzDl6fCx13a7GxzNePMQ9zN3B",
+            },
+            body: JSON.stringify(reqBody),
+          }
+        );
+
+          const data = await res.json();
+          const doesUserExists = data.document
     // const doesUserExists = await User.findOne({ email });
-    // if (!doesUserExists)
-    //   return res.status(400).json({ error: "Invalid email or password" });
+    if (!doesUserExists)
+      return res.status(400).json({ error: "Invalid email or password" });
 
-    // const doesPasswordMatch = await bcrypt.compare(
-    //   password,
-    //   doesUserExists.password
-    // );
+    const doesPasswordMatch = await bcrypt.compare(
+      password,
+      doesUserExists.password
+    );
 
-    // if (!doesPasswordMatch)
-    //   return res.status(400).json({ error: "Invalid email or password" });
+    if (!doesPasswordMatch)
+      return res.status(400).json({ error: "Invalid email or password" });
     
-    //   const payload = { _id: doesUserExists._id };
+      const payload = { _id: doesUserExists._id };
 
-    //   const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    //     expiresIn: "1h",
-    //   });
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
 
-    //   const user = { ...doesUserExists._doc, password: undefined };
-    //   return res.status(200).json({ token, user });
-      return res.status(200).json("Hello from login");
+      const user = { ...doesUserExists._doc, password: undefined };
+      return res.status(200).json({ token, user });
+    //   return res.status(200).json("Hello from login");
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
