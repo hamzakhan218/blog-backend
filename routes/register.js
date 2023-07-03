0340dc5935fb6e76aed8fddbe59d00126c837ad8
Hamza Bashir <hamzabashir218@gmail.com>
2023-07-03 17:10:22 +0500

    Testing register api 7

--- routes/register.js
+++ routes/register.js
@@ -73,7 +73,7 @@ router.post("/register", async (req, res) => {
       }
     );
     const insertedID = await savingDoc.json();
-    return res.status(201).json({ insertedID });
+    return res.status(201).json( insertedID.insertedId );
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
